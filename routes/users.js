e01db506ede8f303ae5409c8e68af6cbdb0e957f
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-02-12 21:25:49 -0800

    Added Register, Update and Current user routs

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,60 @@
 /*
  * All routes for Users are defined here
- * Since this file is loaded in server.js into /users,
+ * Since this file is loaded in server.js into api/users,
  *   these routes are mounted onto /users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const { addUser, updateUser } = require('./helperFunctions');
 
-router.get('/', (req, res) => {
-  res.render('users');
-}); 
+module.exports = (db) => {
+  // Get current users information
+  router.get("/", (req, res) => {
+    const userID = req.session.user_id;
+    if (userID) {
+      db.query(`SELECT * FROM users WHERE id = $1;`, [userID])
+        .then(data => {
+          const users = data.rows[0];
+          res.send({ id: users.id, name: users.name, email: users.email, location: users.location });
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    } else {
+      res.send({});
+    }
+  });
 
-module.exports = router;
+  // Registration route
+  router.post("/register", (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 12);
+    addUser(user, db)
+      .then(user => {
+        req.session.user_id = user.id;
+        res.send(user);
+      })
+      .catch(err => res.send(err));
+  });
+
+  // Update user route
+  router.put("/update", (req, res) => {
+    const userID = req.session.user_id;
+    const obj = req.body;
+    if (obj.password) {
+      obj.password = bcrypt.hashSync(obj.password, 12);
+    }
+    updateUser(obj, userID, db)
+      .then(user => {
+        res.send(user);
+      })
+      .catch(err => res.send(err));
+  });
+
+  return router;
+};
